ff9c641f30357242b82d5f558d96f3260337881e
mikayilov2006@gmail.com <mikayilov2006@gmail.com>
2024-10-20 12:32:48 +0400

    useEffect

--- Useeffect/src/App.jsx
+++ Useeffect/src/App.jsx
@@ -1,10 +1,16 @@
 import React, { useState } from 'react'
-
+import { useEffect } from 'react'
 const App = () => {
-  const [num,setNum] = useState(0)
-  const add = () =>{
-    setNum(num+1)
+  const [num, setNum] = useState(0)
+  const add = () => {
+    setNum(num + 1)
   }
+
+  useEffect(() => {
+    if (num === 10) {
+      console.log("number is 10")
+    }
+  }, [num])
   return (
     <div>
       <h1>{num}</h1>
